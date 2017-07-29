0aab924b03add7ca58593eca1d1af2896632a63e
Unknown <mohammadkhani.ahmad53@gmail.com>
2017-07-29 15:16:40 +0430

    sec

---
+++ src/main/java/Receiver.java
@@ -0,0 +1,35 @@
+/**
+ * Created by QotboddiniH on 7/26/17.
+ */
+import com.satori.rtm.*;
+import com.satori.rtm.model.*;
+
+public class Receiver { //SubscribeToOpenChannel
+    static final String endpoint = "wss://open-data.api.satori.com";
+    static final String appkey = "21320cC01D7bC9364399e01eC8dDFa9C";
+    static final String channel = "github-events";
+
+    public static void main(String[] args) throws InterruptedException {
+        final RtmClient client = new RtmClientBuilder(endpoint, appkey).setListener(new RtmClientAdapter() {
+            @Override
+            public void onEnterConnected(RtmClient client) {
+                System.out.println("Connected to Satori RTM!");
+            }
+        }).build();
+
+        SubscriptionAdapter listener = new SubscriptionAdapter() {
+            @Override
+            public void onSubscriptionData(SubscriptionData data) {
+                int cnt = 0;
+                for (AnyJson json : data.getMessages()) {
+
+                    System.out.println(json.toString() + "\n" + cnt++ + "\n");
+                }
+            }
+        };
+
+        client.createSubscription(channel, SubscriptionMode.SIMPLE, listener);
+
+        client.start();
+    }
+}
